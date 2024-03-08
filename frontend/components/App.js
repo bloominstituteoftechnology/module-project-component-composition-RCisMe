d6535a442a24f7707db28b4ec9bea78a9584a2bd
Bubblesz Hernandez <bubbleszhernandez@Hearts-MacBook-Pro.local>
2024-03-08 18:14:18 -0500

    “UPDATED”

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react' 
 import axios from 'axios'
-import Card from 'Card'
+import Card from './Card'
 
 const api_key = 'DEMO_KEY'
 const URL = `http://api.nasa.gov/planetary/apod?api_key=${api_key}`
